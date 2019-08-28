1a84905350d328d90e8d5369a279752366913667
Erin Gurley <erinmgurley@gmail.com>
2019-08-28 09:35:26 -0700

    updated wordsSelectors to include getSearchTerm

--- src/selectors/wordsSelectors.js
+++ src/selectors/wordsSelectors.js
@@ -1,3 +1,5 @@
 const getWords = state => state.words.dictionary;
 
 export const getFirstWords = (state, count) => getWords(state).slice(0, count);
+
+export const getSearchTerm = state => state.words.searchTerm;
