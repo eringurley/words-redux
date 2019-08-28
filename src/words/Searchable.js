add1045dca99c95b0f6527076b6620f6ae0fb900
Erin Gurley <erinmgurley@gmail.com>
2019-08-28 11:25:11 -0700

    added searchable file

---
+++ src/words/Searchable.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import SearchWords from '../containers/words/SearchWords';
+import TopWords from '../containers/words/TopWords';
+import PropTypes from 'prop-types';
+
+function Searchable({ match }) {
+  return (
+    <>
+      <SearchWords />
+      <TopWords count={match.params.count} />
+    </>
+  );
+}
+
+Searchable.propTypes = {
+  match: PropTypes.object.isRequired
+};
+
+export default Searchable;
+
