714c39a12ba3e3a481cb48014072bed072364902
Erin Gurley <erinmgurley@gmail.com>
2019-08-28 11:25:50 -0700

    wrote getTopWords byTERm to  selectors

--- src/selectors/wordsSelectors.js
+++ src/selectors/wordsSelectors.js
@@ -3,3 +3,10 @@ const getWords = state => state.words.dictionary;
 export const getFirstWords = (state, count) => getWords(state).slice(0, count);
 
 export const getSearchTerm = state => state.words.searchTerm;
+
+export const getTopWordsByTerm = (state, count) => {
+  const searchTerm = getSearchTerm(state);
+  return getWords(state).filter(word => {
+    return word.includes(searchTerm);
+  }).slice(0, count);
+};
