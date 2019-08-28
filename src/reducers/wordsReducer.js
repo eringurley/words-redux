eb827f167d1be043a25189813aaceb46858cf0a0
Erin Gurley <erinmgurley@gmail.com>
2019-08-27 17:13:54 -0700

    updated topWords to include searchTerm

--- src/reducers/wordsReducer.js
+++ src/reducers/wordsReducer.js
@@ -1,4 +1,5 @@
 import words from '../fixtures/words.json';
+import { UPDATE_SEARCH_TERM } from '../actions/wordsActions';
 
 const initialState = {
   dictionary: [...words]
@@ -6,6 +7,8 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   switch(action.type) {
+    case UPDATE_SEARCH_TERM: 
+      return { ...state, searchTerm: action.payload };
     default: return state;
   }
 }
